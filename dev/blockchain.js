5c5a1f200a77528821798051a6f34e14cc402771
1bitcode <38600431+hanbit10@users.noreply.github.com>
2025-03-15 01:51:56 +0000

    update

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,12 +1,45 @@
 class Blockchain {
     constructor () {
         this.chain = []
-        this.newTransactions = []
+        this.pendingTransactions = []
 
         Blockchain.prototype.createNewBlock = (nonce, previousBlockHash, hash) => {
+            const newBlock = {
+                index: this.chain.length + 1,
+                timestamp: Date.now(),
+                transactions: this.pendingTransactions,
+                nonce: nonce,
+                hash: hash,
+                previousBlockHash: previousBlockHash,
+            }
+            this.pendingTransactions = [];
+            this.chain.push(newBlock);
+            return newBlock
+        }
 
+        Blockchain.prototype.getLastBlock = () => {
+            return this.chain[this.chain.length -1]
+        }
+
+        Blockchain.prototype.createTransaction = (amount, sender, recipient) => {
+            const newTransaction = {
+                amount: amount,
+                sender: sender,
+                recipient: recipient
+            }
+
+            this.pendingTransactions.push(newTransaction)
+            return this.getLastBlock()['index'] + 1;
+        }
+
+        Blockchain.prototype.hashBlock = (blockdata) => {
+            
         }
     }
 
 
+
+
 }
+
+module.exports = Blockchain
